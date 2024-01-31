4212a902b840673059f445df1362e7e397c882c8
Lucas <lucasemiliano619@gmail.com>
2024-01-31 01:28:15 -0300

    Galeria responsiva

--- site/script.js
+++ site/script.js
@@ -46,34 +46,77 @@ function clickHamburger() {
 }
 
 function fechaHamburger() {
-  for (let i = 0; i < opcaoNavFixo.length; i++) {
-    opcaoNavFixo[i].style.display = "none";
+  if (screen.width <= 480) {
+    for (let i = 0; i < opcaoNavFixo.length; i++) {
+      opcaoNavFixo[i].style.display = "none";
+    }
+    hamburger.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    brasao.style.marginBottom = "-3.5rem";
   }
-  hamburger.innerHTML = '<i class="fa-solid fa-bars"></i>';
-  brasao.style.marginBottom = "-3.5rem";
+}
+
+// Galeria:
+let galeria = document.getElementById("galeria");
+let galeria1 = document.getElementById("galeria1");
+let galeria2 = document.getElementById("galeria2");
+let galeria3 = document.getElementById("galeria3");
+let galeria4 = document.getElementById("galeria4");
+let galeria5 = document.getElementById("galeria5");
+let preCarregamento = document.getElementById("preCarregamento");
+
+let imagem = [];
+let qntdImg = 10; // Quantidade de imagens na pasta galeria
+let galeriaCompleta = [];
+for (let i = 1; i <= qntdImg; i++) {
+  imagem.push("img/galeria/img" + i + ".webp");
+  galeriaCompleta.push(`url(img/galeria/img${i}.webp)`);
+}
+galeriaCompleta.shift();
+preCarregamento.style.backgroundImage = galeriaCompleta; // Faz um pré carregamento das imagens da galeria
+
+function voltar() {
+  let fim = `${imagem[imagem.length - 1]}`;
+  imagem.pop();
+  imagem.unshift(fim);
+  galeria1.style.backgroundImage = `url(${imagem[0]})`;
+  galeria2.style.backgroundImage = `url(${imagem[1]})`;
+  galeria3.style.backgroundImage = `url(${imagem[2]})`;
+  galeria4.style.backgroundImage = `url(${imagem[3]})`;
+  galeria5.style.backgroundImage = `url(${imagem[4]})`;
+}
+
+function passar() {
+  let inicio = imagem[0];
+  imagem.shift();
+  imagem.push(inicio);
+  galeria1.style.backgroundImage = `url(${imagem[0]})`;
+  galeria2.style.backgroundImage = `url(${imagem[1]})`;
+  galeria3.style.backgroundImage = `url(${imagem[2]})`;
+  galeria4.style.backgroundImage = `url(${imagem[3]})`;
+  galeria5.style.backgroundImage = `url(${imagem[4]})`;
 }
 
 // Botões História:
-let seta = document.querySelectorAll(".fa-solid");
-let ideologia = document.getElementById("ideologia");
-let missao = document.getElementById("missao");
+// let seta = document.querySelectorAll(".fa-solid");
+// let ideologia = document.getElementById("ideologia");
+// let missao = document.getElementById("missao");
 
-function exibirHistoria(i) {
-  if (i === 0) {
-    if (ideologia.style.display == "none") {
-      ideologia.style.display = "block";
-      seta[0].classList.replace("fa-caret-down", "fa-caret-up");
-    } else {
-      ideologia.style.display = "none";
-      seta[0].classList.replace("fa-caret-up", "fa-caret-down");
-    }
-  } else {
-    if (missao.style.display == "none") {
-      missao.style.display = "block";
-      seta[1].classList.replace("fa-caret-down", "fa-caret-up");
-    } else {
-      missao.style.display = "none";
-      seta[1].classList.replace("fa-caret-up", "fa-caret-down");
-    }
-  }
-}
+// function exibirHistoria(i) {
+//   if (i === 0) {
+//     if (ideologia.style.display == "none") {
+//       ideologia.style.display = "block";
+//       seta[0].classList.replace("fa-caret-down", "fa-caret-up");
+//     } else {
+//       ideologia.style.display = "none";
+//       seta[0].classList.replace("fa-caret-up", "fa-caret-down");
+//     }
+//   } else {
+//     if (missao.style.display == "none") {
+//       missao.style.display = "block";
+//       seta[1].classList.replace("fa-caret-down", "fa-caret-up");
+//     } else {
+//       missao.style.display = "none";
+//       seta[1].classList.replace("fa-caret-up", "fa-caret-down");
+//     }
+//   }
+// }
